9bab7cc756e8aeee388b3614a50c624f94c81def
Mustafa Demirhan <mustafademirhan07@gmail.com>
2025-05-27 13:43:38 -0400

    program.cs edited

--- Mylo.Web/Program.cs
+++ Mylo.Web/Program.cs
@@ -7,7 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.IsDevelopment() ? "https://localhost:44343/" : "https://www.mylolaw.work")
