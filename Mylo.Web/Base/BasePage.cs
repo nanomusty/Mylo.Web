1f8c7d01f60c432681fb6d9729f38d06e8a6363d
Mustafa Demirhan <mustafademirhan07@gmail.com>
2025-05-27 15:07:09 -0400

    mainlayout edited

---
+++ Mylo.Web/Base/BasePage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+
+namespace Mylo.Web.Base
+{
+    public class BasePage
+    {
+        [Inject] public IJSRuntime IJSRuntime { get; set; }
+        [Inject] public ISnackbar Snackbar { get; set; }
+        [Inject] public IDialogService DialogService { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        public BasePage(NavigationManager navigationManager, IJSRuntime jSRuntime,
+            ISnackbar snackbar, IDialogService iDialogService)
+        {
+            NavigationManager = navigationManager;
+            IJSRuntime = jSRuntime;
+            Snackbar = snackbar;
+            DialogService = iDialogService;
+        }
+    }
+}
--- Mylo.Web/Program.cs
+++ Mylo.Web/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Mylo.Web;
+using Mylo.Web.Base;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +12,8 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.IsDevelopment() ? "https://localhost:44343/" : "https://www.mylolaw.work")
 });
+
+builder.Services.AddScoped<BasePage>();
 
 builder.Services.AddMudServices();
 
