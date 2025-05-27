3220556a10d9e1657facd63760d5e1c4a24b4ef3
Mustafa Demirhan <mustafademirhan07@gmail.com>
2025-05-27 08:49:40 -0400

    Add project files.

---
+++ Mylo.Web/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Mylo.Web;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.IsDevelopment() ? "https://localhost:44343/" : "https://www.mylolaw.work")
+});
+
+await builder.Build().RunAsync();
