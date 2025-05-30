88b5943b5d6bdde9dd559c77fc65f87448bfa2a1
Mustafa Demirhan <mustafademirhan07@gmail.com>
2025-05-30 10:51:23 -0400

    API  Address changed

--- Mylo.Web/Program.cs
+++ Mylo.Web/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddScoped<BasePage>();
 builder.Services.AddScoped<SessionInfo>();
 builder.Services.AddHttpClient("Wasm", client =>
 {
-    client.BaseAddress = new Uri(builder.HostEnvironment.IsDevelopment() ? "https://localhost:44343/" : "https://mylolaw.work/");
+    client.BaseAddress = new Uri(builder.HostEnvironment.IsDevelopment() ? "https://localhost:44343/" : "https://nanomusty-001-site1.qtempurl.com/");
     // client.DefaultRequestHeaders.Add("Accept", "application/json");
 });
 builder.Services.AddBlazoredLocalStorage();
