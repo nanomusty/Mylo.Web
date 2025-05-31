3b478b75edb9c41ccad8affed7768c65962faff4
Mustafa Demirhan <mustafademirhan07@gmail.com>
2025-05-30 21:32:10 -0400

    User Dialog edited

---
+++ Mylo.Web/ObjectModels/ContactCard.cs
@@ -0,0 +1,17 @@
+﻿namespace Mylo.Web.ObjectModels
+{
+    public class ContactCard : BaseEntity
+    {
+        public Guid OrganisationId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Company { get; set; }
+        public string? JobTitle { get; set; }
+        public string? Address { get; set; }
+        public string? Website { get; set; }
+        public string? Notes { get; set; }
+        public string? ProfileImageUrl { get; set; }
+    }
+}
--- Mylo.Web/Program.cs
+++ Mylo.Web/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddScoped<SessionInfo>();
 builder.Services.AddHttpClient("Wasm", client =>
 {
     client.BaseAddress = new Uri(builder.HostEnvironment.IsDevelopment() ? "https://localhost:44343/" : "https://nanomusty-001-site1.qtempurl.com/");
-    // client.DefaultRequestHeaders.Add("Accept", "application/json");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
 });
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
