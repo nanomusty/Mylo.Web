568c34b9077bb7e59a30da095c58ea11ef918889
Mustafa Demirhan <mustafademirhan07@gmail.com>
2025-06-02 21:50:18 -0400

    register page completed

---
+++ Mylo.Web/ObjectModels/OrganisationRegisterModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Mylo.Web.ObjectModels
+{
+    public class OrganisationRegisterModel 
+    {
+        // Organization Details
+        public string OrganisationName { get; set; }
+
+        // Admin User Details
+        public string AdminFirstName { get; set; }
+        public string AdminLastName { get; set; }
+        public string AdminEmail { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        // Terms and Privacy
+        public bool AcceptTerms { get; set; }
+        public bool AcceptPrivacy { get; set; }
+    }
+}
